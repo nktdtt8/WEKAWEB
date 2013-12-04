0224048dea7a5a4675b4b1396eb8e2ad7bdfd742
aniket <aniketdutta@outlook.com>
2013-12-04 22:33:37 +0530

    added more classes

---
+++ WekaWS/src/org/du/dm/model/dao/FileModelBase.java
@@ -0,0 +1,41 @@
+package org.du.dm.model.dao;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+
+import org.du.dm.dao.DatabaseDao;
+import org.du.dm.model.Model;
+import org.du.dm.model.ModelList;
+
+public class FileModelBase implements DatabaseDao<Model> {
+
+	private ModelList models;
+	private BufferedReader reader;
+		
+	public FileModelBase(InputStream fileStream) {
+		init();
+	}
+	
+	private void init() {
+		//[TODO load models from csv file ]
+	}
+	@Override
+	public Model get(Model i) {
+		// TODO implement
+		return models.get(i);
+	}
+
+	@Override
+	public void modify(Model m1, Model m2) {
+		// TODO Auto-generated method stub
+		models.remove(m1);
+		models.add(m2);
+	}
+
+	@Override
+	public void insert(Model m) {
+		// TODO Auto-generated method stub
+		models.add(m);
+	}
+
+}
