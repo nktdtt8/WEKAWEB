5017a72db0078faaf871f661820dba483027585e
aniket <aniketdutta@outlook.com>
2013-12-06 13:34:26 +0530

    added TODO

--- WekaWS/src/org/du/dm/io/CsvFileModel.java
+++ WekaWS/src/org/du/dm/io/CsvFileModel.java
@@ -1,12 +1,20 @@
 package org.du.dm.io;
 
+import java.io.BufferedReader;
+
 import org.du.dm.model.ModelList;
 
 public class CsvFileModel implements ModelConverterInterface {
 
+	private BufferedReader br;
+	
+	public CsvFileModel(String fileName) {
+		//[TODO] load Reader here
+	}
 	@Override
 	public ModelList convert() {
 		// TODO Auto-generated method stub
+		//[TODO] create Models and return here
 		return null;
 	}
 
