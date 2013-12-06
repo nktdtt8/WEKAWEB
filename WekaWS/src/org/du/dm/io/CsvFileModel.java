366a520e93898ca9bea290bcde736dde7300248c
aniket <aniketdutta@outlook.com>
2013-12-06 13:32:24 +0530

    added classes

---
+++ WekaWS/src/org/du/dm/io/CsvFileModel.java
@@ -0,0 +1,13 @@
+package org.du.dm.io;
+
+import org.du.dm.model.ModelList;
+
+public class CsvFileModel implements ModelConverterInterface {
+
+	@Override
+	public ModelList convert() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ WekaWS/src/org/du/dm/io/ModelConverterInterface.java
@@ -0,0 +1,9 @@
+package org.du.dm.io;
+
+import org.du.dm.model.ModelList;
+
+public interface ModelConverterInterface {
+	
+	public ModelList convert();
+
+}
