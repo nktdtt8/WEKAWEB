93ff69a97e165a16729104092e02339f61b9928d
aniket <aniket@aniket-Dell-System-Vostro-3450>
2014-01-07 02:12:22 +0530

    BUG FIXED (JS SYNTAX ERROR)

--- MyWebService/WebContent/WEB-INF/upload/data.js
+++ MyWebService/WebContent/WEB-INF/upload/data.js
@@ -2,43 +2,27 @@
  * this javascript does validation and calls data rest service
  */
 
-function myfunc() {
+
+function myfunc() 
+{
 			
-		alert(document.getElementById("element_1").value);
-		// create new FormData
-		var formData = getFormData();		
+		alert('hello');
 		
 		/**
-		*   calling REST request from javascript
-		*
+		*	generate form data
 		*/
-		var c = new XMLHttpRequest();
-		
-		c.open("POST","http://localhost:8080/MyWebService/rest/data/upload",false);
-		c.send(formData);
-
-		if(c.status == 200)
-			alert(c.responseText);
-		else
-			alert(c.status + ":" + c.statusText);
-				
-		// redirect to main page if successfully done
-		
-		window.location.href = "Home.htm";
-	}
-	
-	/**
-	 * generate form data compliant to the WebService this will mock a form
-	 */
-	function getFormData() {
 		var fileName = document.getElementById("element_1").value;
 		var desc = document.getElementById("element_2").value;
 		var file = document.getElementById("element_3").files[0];
 		var dd = document.getElementById("element_4_1").value;
 		var mm = document.getElementById("element_4_2").value;
 		var yy = document.getElementById("element_4_3").value;
-		String date = yy.concat('/',mm,'/',dd);
-		alert(date);
+		//create date
+		yy = yy.concat("/");
+		yy = yy.concat(mm);
+		yy = yy.concat("/");
+		var date = yy.concat(dd);
+		
 		var type_csv = document.getElementById("element_5_1");
 		var type = 0;
 		if(type_csv.checked)
@@ -65,5 +49,25 @@ function myfunc() {
 		data.append("isLabeled",label);
 		data.append("desc",desc);
 		data.append("uploaddate",date);
-		return data;
-	};
+	
+		/**
+		*   calling REST request from javascript
+		*
+		*/
+		var c = new XMLHttpRequest();
+		
+		c.open("POST","http://localhost:8080/MyWebService/rest/data/upload",false);
+		c.send(data);
+
+		if(c.status == 200)
+			alert(c.responseText);
+		else
+			alert(c.status + ":" + c.statusText);
+				
+		// redirect to main page if successfully done
+		
+		window.location.href = "Home.htm";
+		
+}
+	
+	
